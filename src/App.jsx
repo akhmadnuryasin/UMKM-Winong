acfd19c501094d0892e58c9f5a09a020e23aa426
Akhmad Nuryasin <akhmadnuryasin18@gmail.com>
2024-01-25 23:07:16 +0700

    change duration modal open

--- src/App.jsx
+++ src/App.jsx
@@ -138,7 +138,7 @@ function App() {
                 initial={{ opacity: 0 }}
                 animate={{ opacity: 1 }}
                 exit={{ opacity: 0 }}
-                transition={{ duration: 0.5 }}
+                transition={{ duration: 0 }}
               >
                 <Modal
                   isOpen={isOpen}
